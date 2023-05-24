c7b54e65d3f91dbafb5d43f98b515c2611cb3d38
CHIH-WEN CHEN <133675064+asdy9423@users.noreply.github.com>
2023-05-25 01:21:06 +0800

    Add files via upload

---
+++ map.js
@@ -0,0 +1,27 @@
+$(".yizhu").mouseenter(function () {
+  $(".img1").css("display", "block");
+})
+$(".yizhu").mouseleave(function () {
+  $(".img1").css("display", "none");
+})
+
+$(".budai").mouseenter(function () {
+  $(".img2").css("display", "block");
+})
+$(".budai").mouseleave(function () {
+  $(".img2").css("display", "none");
+})
+
+$(".puzi").mouseenter(function () {
+  $(".img3").css("display", "block");
+})
+$(".puzi").mouseleave(function () {
+  $(".img3").css("display", "none");
+})
+
+$(".yanshui").mouseenter(function () {
+  $(".img4").css("display", "block");
+})
+$(".yanshui").mouseleave(function () {
+  $(".img4").css("display", "none");
+})
---
+++ trip.js
@@ -0,0 +1,9 @@
+function change1() {
+  document.getElementById("photo").src = "PICTURE/000001.png";
+}
+function change2() {
+  document.getElementById("photo").src = "PICTURE/000002.png";
+}
+function change3() {
+  document.getElementById("photo").src = "PICTURE/000003.png";
+}
